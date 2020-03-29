0edffae4794cf99aae355a89acbc37eb45cd0e0d
Ilias-Karimalis <iliaskarimalis@outlook.com>
2020-03-28 21:14:59 -0700

    completed bst.c

--- data_structures/bst.c
+++ data_structures/bst.c
@@ -1,3 +1,5 @@
+#include "stack.h"
+
 #define NULL ((void*) 0)
 
 typedef struct Node {
@@ -10,9 +12,9 @@ Node* node_init(int value);
 void bst_insert(Node* bst, int value);
 void bst_remove(Node* bst, int value);
 Node* bst_find(Node* bst, int value);
-void bst_traverse(Node* bst);
+void bst_inorder(Node* bst);
 void bst_delete(Node* bst);
-void bst_print(Node* bst);
+Node* bst_minValueNode(Node* bst);
 
 Node* node_init(int value) {
     Node* res = malloc(sizeof(Node));
@@ -33,24 +35,64 @@ void bst_insert(Node* bst, int value) {
     }
 }
 
-void bst_remove(Node* bst, int value) {
-    if (bst) {
-        if (bst->value == value) {
-            
-        } else if (bst->value > value) {
-            bst_remove(bst->left, value);
-        } else {
-            bst_remove(bst->right, value);
-        }
-    }
-}
-
 Node* bst_find(Node* bst, int value) {
     if (!bst) {
         return NULL;
     } else {
         if (bst->value == value) {
-            
+            return bst;
+        } else if (bst->value > value) {
+            return bst_find(bst->left, value);
+        } else {
+            return bst_find(bst->right, value);
         }
+    }
+}
+
+void bst_inorder(Node* bst) {
+    if (root) {
+        bst_inorder(bst->left);
+        printf("%d \n", bst->key);
+        bst_inorder(bst->right);
+    }
+}
+
+Node* bst_remove(Node* bst, int value) {
+    if (!root) {
+        return root;
+    } else if (bst->value > value) {
+        bst->left = bst_remove(bst->left, value);
+    } else if (bst->value < value) {
+        bst->right = bst_remove(bst->right, value);
+    } else {
+        if (!bst->left) {
+            Node* temp = root->right;
+            free(bst);
+            return temp;
+        } else if (!bst->right) {
+            Node* temp = root->left;
+            free(bst);
+            return temp;
+        } else {
+            Node* temp = bst_minValueNode(bst->right);
+            bst->value = temp->value;
+            bst->right = bst_remove(bst->right, temp->value);
+        }
+    }
+    return bst;
+}
+
+Node* bst_minValueNode(Node* bst) {
+    Node* curr = bst;
+    while(curr && curr->left)
+        curr = curr->left;
+    return curr;
+}
+
+void bst_delete(Node* bst) {
+    if (bst) {
+        bst_delete(bst->left);
+        bst_delete(bst->right);
+        free (bst);
     }
 }
